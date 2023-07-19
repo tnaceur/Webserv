89e456893729e585bf38115112b3fd38c313a6aa
Taha Naceur <tnaceur@e1r3p7.1337.ma>
2023-07-19 08:52:41 +0100

    Webserv

--- main.cpp
+++ main.cpp
@@ -6,23 +6,40 @@
 
 int  main(int ac, char **av)
 {
-    /* socket() creat new socket and return the socket descriptor */
     int web_socket = socket(AF_INET, SOCK_STREAM, 0);
+    /* socket() creat new socket and return the socket descriptor */
     if (web_socket < 0)
         return (void)(std::cerr << "socket error: \n"), 1;
     /* sockaddr_in is a sturct that contain information about the server it's 'ip, port' ... */
     sockaddr_in addr;
     addr.sin_family = AF_INET;
-    if (inet_pton(AF_INET, "10.11.2.9", &(addr.sin_addr)) < 0)
+    if (inet_pton(AF_INET, "127.0.0.1", &(addr.sin_addr)) < 0)
         return (void)(std::cerr << "Invalid address.\n"), 1;
-    addr.sin_port = htons(5555);
+    addr.sin_port = htons(8888);
     if (bind(web_socket, (struct sockaddr*)(&addr), sizeof(addr)) < 0)
         return (void)(perror(NULL)), 1;
     listen(web_socket, 10);
-    int client = accept(web_socket, NULL, NULL);
-    send(client, "Hello world\n", 12, 0);
-    close(web_socket);
+    int client;
+    sockaddr_in clientAddr;
+    socklen_t client_lenght = sizeof(clientAddr);
+    while (1)
+    {
+       client = accept(web_socket, (struct sockaddr*)(&addr), &client_lenght);
+       const char * response = "HTTP/1.1 200 OK\r\n"
+                          "Content-Type: text/html\r\n"
+                          "Content-Length: 12\r\n"
+                          "\r\n"
+                          "Hello, World!";
+        if (client < 0) {
+            std::cerr << "Failed to accept connection." << std::endl;
+            return 1;
+        }
+        else
+           send(client, response, strlen(response), 0);
+    }
     close(client);
+    close(web_socket);
     return 0;
     
 }
+
